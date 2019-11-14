922ecd522acea615f6bd8a6454acdd8cda7b9597
David Randell <davidrandell@Davids-MacBook-Pro.local>
2019-11-14 15:33:48 +0000

    Boolean chanllenge

--- booleans.js
+++ booleans.js
@@ -1,3 +1,23 @@
-var a = 10
-var b = 20
-console.log(a>b)
+
+
+let temp = 180
+
+if (temp <= 32) {
+    console.log("It is Freezing outside")
+} 
+
+if (temp >=120) {
+    console.log("It is way too hot.")
+}
+
+// Boolean Challenge
+
+let age = 32
+
+if(age <= 7){
+    console.log("You qualify for the Kids discount")
+}
+
+if (age >= 65){
+    console.log("You qualify for the Seniors discount")
+}
