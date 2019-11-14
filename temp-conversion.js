0a8e69f1e2337610636d0284007cebdbafa64fd3
David Randell <davidrandell@Davids-MacBook-Pro.local>
2019-11-14 15:02:37 +0000

    completed temp-conversion

---
+++ temp-conversion.js
@@ -0,0 +1,9 @@
+let fahrenheit = 64
+
+//Convert to Cel
+let celcius = (fahrenheit - 32) * 5/9
+console.log(celcius)
+
+//Convert to Kelvin
+let kelvin = (fahrenheit + 459.67)* 5/9
+console.log(kelvin)
