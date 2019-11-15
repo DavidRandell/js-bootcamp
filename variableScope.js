d7c451bafc5f589bfe37442b6ac1078f8ebd13a0
David Randell <davidrandell@Davids-MacBook-Pro.local>
2019-11-15 17:22:53 +0000

    Added Scope Part 1

--- variableScope.js
+++ variableScope.js
@@ -4,12 +4,26 @@
 // In this example the if statement creates the only code block, this is in the local scope.
     // So in this example varTwo is in the Local scope, the console.log on line 15 is calling from the Global Scope therefore varTwo does not exist.
 
+// Global Scope (varOne)
+    // Local Scope (varTwo)
+        // Local Scope (varFour)
+    // Local Scope (varThree)
+
+
 let varOne = "VarOne"
 
 if (true) {
     console.log(varOne)
     let varTwo = "VarTwo"
     console.log(varTwo)
+
+    if (true){
+        let varFour = "varFour"
+    }
+}
+
+if (true){
+    let varThree = "varThree"
 }
 
 console.log(varTwo)
