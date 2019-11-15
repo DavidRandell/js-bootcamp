207f07f00ddcdac9a256ff0a75c4cbd16e03d9ff
David Randell <davidrandell@Davids-MacBook-Pro.local>
2019-11-15 17:02:12 +0000

    Added Challenge

--- logical-and-or.js
+++ logical-and-or.js
@@ -7,3 +7,15 @@ if (temp >= 32 && temp <= 80) {
 if (temp < 0 || temp > 100){
     console.log('Do not go outside!')
 }
+
+// Challenge Area
+let isGuestOneVegan = false
+let isGuestTwoVegan = false
+
+if (isGuestOneVegan && isGuestTwoVegan){
+    console.log("Only offer up Vegan")
+} else if(isGuestOneVegan || isGuestTwoVegan){
+    console.log("Offer up some Vegan options")
+} else {
+    console.log("Offer up anything")
+}
